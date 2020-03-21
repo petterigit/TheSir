829f5543c1d79f0b36af393fb9300b17e36ff38a
Maija <maijaaaw@gmail.com>
2020-03-21 02:06:32 +0200

    transale and parse title text

--- scripts/niiloTweet/main.js
+++ scripts/niiloTweet/main.js
@@ -25,6 +25,9 @@ const parameters = {
 
 exports.getNiiloTweet = async message => {
   try {
+    //const year = randomNumber(2012, 2020);
+    const year = "2015";
+
     T.get(path, parameters, function(err, data, response) {
       const tweet = getRandomTweet(data);
       sendTweetToDiscord(tweet, message);
@@ -35,21 +38,124 @@ exports.getNiiloTweet = async message => {
   }
 };
 
-function sendTweetToDiscord(tweet, message) {
-  const embed = {
-    description: "Niilo22 twiittasi " + tweet.created_at + ":\n" + tweet.text
-  };
-  message.channel.send({ embed: embed });
-}
-
 function getRandomTweet(tweets) {
   return tweets[randomNumber(0, MAX_TWEETS)];
 }
 
-// Both ends inclusive
 const randomNumber = (start, end) => {
   return Math.floor(Math.random() * (start - end + 1)) + end;
 };
+
+function sendTweetToDiscord(tweet, message) {
+  const embed = createAnswerMessage(tweet);
+  message.channel.send({ embed: embed });
+}
+
+function createAnswerMessage(tweet) {
+  let answerMessage = {
+    description:
+      "Niilo22 twiittasi " + parseDate(tweet.created_at) + ":\n\n" + tweet.text
+  };
+  return answerMessage;
+}
+
+function parseDate(date) {
+  let parsedDate = "";
+  const weekday = date.slice(0, 3);
+  const month = date.slice(4, 7);
+  const day = date.slice(8, 10);
+  const time = date.slice(11, 16);
+  const year = date.slice(26, 30);
+
+  parsedDate =
+    parseWeekday(weekday) +
+    " " +
+    parseDay(day) +
+    " " +
+    parseMonth(month) +
+    " " +
+    year +
+    " klo " +
+    time;
+
+  return parsedDate;
+}
+
+function parseWeekday(weekday) {
+  let parsedWeekday = "";
+  const weekdaysEng = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const weekdaysFin = [
+    "maanantaina",
+    "tiistaina",
+    "keskiviikkona",
+    "torstaina",
+    "perjantaina",
+    "lauantaina",
+    "sunnuntaina"
+  ];
+
+  for (let i = 0; i < 7; i++) {
+    if (weekday == weekdaysEng[i]) {
+      parsedWeekday = weekdaysFin[i];
+      break;
+    }
+  }
+
+  return parsedWeekday;
+}
+
+function parseMonth(month) {
+  let parsedMonth = "";
+  const monthsEng = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  const monthsFin = [
+    "tammikuuta",
+    "helmikuuta",
+    "maaliskuuta",
+    "huhtikuuta",
+    "toukokuuta",
+    "kesäkuuta",
+    "heinäkuuta",
+    "elokuuta",
+    "syyskuuta",
+    "lokakuuta",
+    "marraskuuta",
+    "joulukuuta"
+  ];
+
+  for (let i = 0; i < 12; i++) {
+    if (month == monthsEng[i]) {
+      parsedMonth = monthsFin[i];
+      break;
+    }
+  }
+
+  return parsedMonth;
+}
+
+function parseDay(day) {
+  let parsedDay = "";
+
+  if (day.slice(0, 1) == "0") {
+    parsedDay = day.slice(1, 2) + ".";
+  } else {
+    parsedDay = day + ".";
+  }
+
+  return parsedDay;
+}
 
 // GET https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
 // GET https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=niilo222&tweet_mode=extended&exclude_replies=true
