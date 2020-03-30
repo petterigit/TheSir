d0624f0f91a8587884ba1eb038a3f4c5f28ae3f4
Petteri Mäkelä <p-mak@pop-os.localdomain>
2020-03-30 18:33:56 +0300

    pop fix

--- scripts/pop/main.js
+++ scripts/pop/main.js
@@ -3,14 +3,14 @@
 
 exports.pop = async (message) => {
     try {
-        let pops = "";
-        let seis = message.guild.emojis.find(emoji => emoji.name === "seis");
+        let pops = "\u2800\n";
+        let seis = "<:seis:690617911748067368>"
         let args = message.content.substring(1).split(" ");
         let amount = args[2];
         if (amount > 0 && amount <= 14) {
             for(let i = 0; i < amount; i++) {
                 for(let i = 0; i < amount; i++) {
-                    pops = pops + "||pop!|| ";
+                    pops = pops + "||pop!||";
                 }
                 pops = pops + "\n";
             }
