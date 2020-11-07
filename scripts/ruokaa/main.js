b8c680f692b6e448816df0ecc28504e2c7e8dcbf
Juho Heiskanen <juho.heiskanen@gmail.com>
2020-11-07 10:57:26 +0200

    Fetch restaurant menus and show them to help with the choice

--- scripts/ruokaa/main.js
+++ scripts/ruokaa/main.js
@@ -1,19 +1,58 @@
-"use strict";
-/* IMPORT const ruokaa = require('./scripts/ruokaa/main.js');
-/* MAIN ruokaa() */
+'use strict';
+
+const fetch = require('node-fetch');
 
 exports.ruokaa = async (message) => {
-    try {
-          const mes = await message.channel.send(`
-Syödään tänään
-🍉 Yololla
-🍑 Laserilla
-`)
-          mes.react("🍉");
-          mes.react("🍑");
+  message.channel.startTyping();
+
+  try {
+    const menu = await fetch(
+      'https://skinfo.juho.space/categories.json'
+    ).then((res) => res.json());
+
+    const texts = [`Syödään tänään`];
+
+    const appendMenu = (categories, header) => {
+      texts.push(header);
+
+      for (const category of categories) {
+        texts.push(` - ${category.category}:`);
+        for (const food of category.foods) {
+          let result = `   ${food.name}`;
+          if (food.dietInfo.length > 0) {
+            result += ` (${food.dietInfo.join('/')})`;
+          }
+          texts.push(result);
         }
-    catch (error) {
-        console.log(error)
+      }
+    };
+
+    const yolo = menu.yolo && menu.yolo.length > 0;
+    const laseri = menu.laseri && menu.laseri.length > 0;
+
+    if (!yolo && !laseri) {
+      await message.channel.send('Ei ruokalistoja.');
+      return;
     }
 
-}
+    if (yolo) {
+      appendMenu(menu.yolo, `🍉 Yololla`);
+    }
+
+    if (laseri) {
+      appendMenu(menu.laseri, `🍑 Laserilla`);
+    }
+
+    const mes = await message.channel.send(texts.join('\n'));
+    if (yolo) {
+      await mes.react('🍉');
+    }
+    if (laseri) {
+      await mes.react('🍑');
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await message.channel.stopTyping();
+  }
+};
