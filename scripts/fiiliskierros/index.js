e1f511daf03a113b84d49d72a4506f6418805b4d
nikugronberg <niku.gronberg@student.lut.fi>
2021-08-22 11:50:30 +0300

    do something to fiiliskierros

--- scripts/fiiliskierros/index.js
+++ scripts/fiiliskierros/index.js
@@ -1,27 +1,30 @@
-/* IMPORT const fiiliskierros = require('./scripts/fiiliskierros/main.js');
-/* MAIN fiiliskierros(message)
- */
-
-
-exports.fiiliskierros = async (message) => {
-
-    let args = message.content.substring(1).split(" ");
-    if (args[2] == "possu") {
-        try {
-
-            const member = message.mentions.members.first();
-            if (member) {
-                message.channel.send(`Possun omistaja on nyt ${member.displayName}, vain hän saa puhua.`);
-            } else {
-                message.channel.send(`Oloni on pettynyt.`)
-                .then(message.channel.send(`Yrittäisit edes.`))
-                .then(message.channel.send(`sir fiilikset possu @member`));
-            }
-        }
-        catch (error) {
-            console.log(error)
-        }
+const fiiliskierros = async (message) => {
+  let args = message.content.substring(1).split(" ");
+  if (args[2] == "possu") {
+    try {
+      const member = message.mentions.members.first();
+      if (member) {
+        message.channel.send(
+          `Possun omistaja on nyt ${member.displayName}, vain hän saa puhua.`
+        );
+      } else {
+        message.channel
+          .send(`Oloni on pettynyt.`)
+          .then(message.channel.send(`Yrittäisit edes.`))
+          .then(message.channel.send(`sir fiilikset possu @member`));
+      }
+    } catch (error) {
+      console.log(error);
     }
-    
+  }
+};
 
-}
+module.exports = {
+  data: {
+    name: "fiiliskierros",
+    description: "Kun on taas aika fiilistellä",
+  },
+  async execute(message) {
+    await fiiliskierros(message);
+  },
+};
