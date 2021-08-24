0464d5bc11b9c10aec49c4f40d8bcd7f56050a21
nikugronberg <niku.gronberg@student.lut.fi>
2021-08-24 17:40:23 +0300

    fix sir poke

--- scripts/poke/index.js
+++ scripts/poke/index.js
@@ -1,21 +1,26 @@
 "use strict";
-/* IMPORT const poke = require('./scripts/poke/main.js');
-/* MAIN poke(message)
- */
 
-
-exports.poke = async (message) => {
-    try {
-        const user = message.mentions.users.first();
-        if (user) {
-            user.send("poke :3");
-            message.delete()
-            .then(message.channel.send(`HERÄTYS ${user.username}, senkin vätys!`))
-            .catch(console.log("No rights to delete messages"));
-        }
+const poke = async (message) => {
+  try {
+    const user = message.mentions.users.first();
+    if (user) {
+      user.send("poke :3");
+      message
+        .delete()
+        .then(message.channel.send(`HERÄTYS ${user.username}, senkin vätys!`))
+        .catch(console.log("No rights to delete messages"));
     }
-    catch (error) {
-        console.log(error)
-    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+module.exports = {
+  data: {
+    name: ["poke"],
+    description: "Poke your friends and family!",
+  },
+  async execute(message) {
+    await poke(message);
+  },
+};
