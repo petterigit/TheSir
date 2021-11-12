9a81fdf9804c153867a98d6a48b34f1e2061f910
Aleksi J <aksu.j@hotmail.com>
2021-11-12 15:23:33 +0200

    Create alpha version of meme generator.

---
+++ slash-commands/mg/MemeGenerator.test.ts
@@ -0,0 +1,45 @@
+import { generateMeme, MemeOptions } from "./MemeGenerator";
+import * as fs from "fs";
+import path from "path";
+
+test("generates drake meme", async () => {
+    const memeOptions: MemeOptions = {
+        name: "drake",
+        type: "SUB_COMMAND",
+        options: [
+            {
+                name: "nah",
+                type: "STRING",
+                value: "Pelaa pelei",
+            },
+            {
+                name: "yes",
+                type: "STRING",
+                value: "Tee random meme-generaattoria jota kukaan ei pyytänyt",
+            },
+        ],
+    };
+    const meme = await generateMeme(memeOptions);
+    fs.writeFileSync(path.join(process.cwd(), "/test/drakeTest.jpg"), meme);
+});
+
+test("generates puh meme", async () => {
+    const memeOptions: MemeOptions = {
+        name: "puhi",
+        type: "SUB_COMMAND",
+        options: [
+            {
+                name: "lame",
+                type: "STRING",
+                value: "Pelaa pelei",
+            },
+            {
+                name: "fancy",
+                type: "STRING",
+                value: "Tee paskaa meme-generaattoria",
+            },
+        ],
+    };
+    const meme = await generateMeme(memeOptions);
+    fs.writeFileSync(path.join(process.cwd(), "/test/puhiTest.jpg"), meme);
+});
