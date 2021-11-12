0f949ffa9f665baf206b6ba74751253287fbd341
AleksiJarventausta <aksu.j@hotmail.com>
2021-11-12 15:38:06 +0200

    Meme generator alpha version (#54)
    * Add public folder.
    
    * Fix build.
    
    * Author Typo
    
    * Procfile.
    
    * init mg.
    
    * Create alpha version of meme generator.
    
    * init mg.
    
    * Create alpha version of meme generator.
    
    Co-authored-by: petterigit <33552590+petterigit@users.noreply.github.com>

---
+++ slash-commands/mg/index.ts
@@ -0,0 +1,38 @@
+import { CommandInteraction, MessageAttachment } from "discord.js";
+import { ApplicationCommandTypes } from "discord.js/typings/enums";
+
+import { getNicknameOrName } from "../../util";
+import { genCommandOptions, generateMeme, MemeOptions } from "./MemeGenerator";
+
+const mg = async (interaction: CommandInteraction) => {
+    await interaction.deferReply();
+    const data = interaction.options.data[0] as unknown as MemeOptions;
+    const memer = getNicknameOrName(interaction);
+
+    try {
+        const buffer = await generateMeme(data);
+        const attachment = new MessageAttachment(buffer, "reaction.jpg");
+        interaction.editReply({
+            content: `${memer} created a ${data.name} meme!`,
+            files: [attachment],
+        });
+        //interaction.editReply({ content: "aaaa" });
+    } catch (error) {
+        interaction.editReply({ content: "Meme generator is broken" });
+        console.log(error);
+        return;
+    }
+};
+
+module.exports = {
+    data: {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: ["mg"],
+        description:
+            "Everyone deserves some praise (or shame) every once in a while",
+        options: genCommandOptions(),
+    },
+    async execute(message: CommandInteraction) {
+        await mg(message);
+    },
+};
