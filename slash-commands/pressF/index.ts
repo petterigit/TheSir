44e24d47e745de60e468497a1bd8ad5293ad04c2
Niku <42005647+Nikug@users.noreply.github.com>
2021-11-12 12:19:19 +0200

    make f to slash command (#52)
    Co-authored-by: nikugronberg <niku.gronberg@student.lut.fi>

---
+++ slash-commands/pressF/index.ts
@@ -0,0 +1,25 @@
+"use strict";
+
+import { ApplicationCommandTypes } from "discord.js/typings/enums";
+import { CommandInteraction } from "discord.js";
+import { getNicknameOrName } from "../../util";
+
+const f = async (interaction: CommandInteraction) => {
+    await interaction.deferReply();
+    const user = getNicknameOrName(interaction);
+    const msg = `${user} has paid their respects`;
+
+    await interaction.editReply(msg);
+    return;
+};
+
+module.exports = {
+    data: {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: ["f"],
+        description: "F in the chat bois",
+    },
+    async execute(interaction: CommandInteraction) {
+        await f(interaction);
+    },
+};
