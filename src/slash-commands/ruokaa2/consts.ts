7f18dbc4ec8b8f24cf0b06790a66bcac7dab5feb
Vilperi <33552590+petterigit@users.noreply.github.com>
2023-02-14 23:05:14 +0200

    first working version (#87)

---
+++ src/slash-commands/ruokaa2/consts.ts
@@ -0,0 +1,53 @@
+export const Restaurant = {
+    yolo: "Yolo",
+    laseri: "Laser",
+    lutBuffet: "LUT Buffet",
+    keskusta: "Keskusta"
+};
+
+export const RestaurantButtons = {
+    ...Restaurant,
+    skip: "Skip",
+};
+
+export const DayMap = {
+    Sunday: 0,
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6,
+};
+
+export const DayChangeHourUtc = 12;
+
+export type FoodConfig = Record<number, string[]>;
+
+export const ConfigFileName = "ruokaa.json";
+
+export const DefaultFoodConfig: FoodConfig = {
+    [DayMap.Monday]: [Restaurant.yolo, Restaurant.laseri, Restaurant.lutBuffet],
+    [DayMap.Tuesday]: [
+        Restaurant.yolo,
+        Restaurant.laseri,
+        Restaurant.lutBuffet,
+    ],
+    [DayMap.Wednesday]: [
+        Restaurant.yolo,
+        Restaurant.laseri,
+        Restaurant.lutBuffet,
+    ],
+    [DayMap.Thursday]: [
+        Restaurant.yolo,
+        Restaurant.laseri,
+        Restaurant.lutBuffet,
+    ],
+    [DayMap.Friday]: [Restaurant.yolo, Restaurant.laseri, Restaurant.lutBuffet],
+    [DayMap.Saturday]: [
+        Restaurant.yolo,
+        Restaurant.laseri,
+        Restaurant.lutBuffet,
+    ],
+    [DayMap.Sunday]: [Restaurant.yolo, Restaurant.laseri, Restaurant.lutBuffet],
+};
