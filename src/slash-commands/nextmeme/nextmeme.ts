9f2068c8b6fa3b59dbdd6813af1bc243fe00846e
Niku Grönberg <gronbergniku@gmail.com>
2024-08-29 17:40:09 +0300

    fetch memes with puppeteer

--- src/slash-commands/nextmeme/nextmeme.ts
+++ src/slash-commands/nextmeme/nextmeme.ts
@@ -1,6 +1,10 @@
 import sample from "lodash/sample";
 import random from "lodash/random";
 import axios from "axios";
+import {
+    launchPuppeteer,
+    navigateToPage,
+} from "../../utils/ruokaa-utils/puppeteerUtils";
 
 const startingDate = new Date(2012, 1, 1);
 
@@ -29,10 +33,25 @@ type JsonResponse = {
 
 export const nextMeme = async (): Promise<Meme> => {
     const timestamp = randomTimestamp(startingDate, new Date());
-    const res = await axios(
-        "https://www.memedroid.com/memes/getGallerySurroundings/" + timestamp
+    const browser = await launchPuppeteer();
+
+    const page = await browser.newPage();
+    await navigateToPage(
+        page,
+        `https://www.memedroid.com/memes/getGallerySurroundings/${timestamp}`
     );
-    const json: JsonResponse = res.data;
+
+    await page.content();
+
+    const json: JsonResponse = await page.evaluate(() => {
+        return JSON.parse(document.querySelector("body").innerText);
+    });
+
+    await browser.close();
+
+    if (!json) {
+        throw new Error("Puppeteer failed to get json.");
+    }
 
     if (json.stat != 0) {
         throw new Error("Status was not 0. Status: " + json.stat);
@@ -50,5 +69,5 @@ export const nextMeme = async (): Promise<Meme> => {
 // Makes a timestamp in Memedroid url format (10 chars long)
 const randomTimestamp = (start: Date, end: Date) => {
     const ranNumber = random(start.getTime(), end.getTime());
-    return ranNumber.toString().substr(0, 10);
+    return ranNumber.toString().substring(0, 10);
 };
